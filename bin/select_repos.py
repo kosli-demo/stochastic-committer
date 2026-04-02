dd4983b329ef08fff7de7d6fb99d854a307c3cbc
JonJagger <jon@kosli.com>
2026-04-02 05:58:33 +0100

    Start work on new design

--- bin/select_repos.py
+++ bin/select_repos.py
@@ -10,34 +10,21 @@ def print_help():
     print("""
         argv[1] == JSON filename containing list of repo names
         argv[2] == number of entries in JSON filename to process
-        argv[3] == chance (1-100) that each entry will be selected
+        argv[3] == %chance (1-100) that each entry will be selected
           
-        Entries that are selected will have an additional field boolean "repo_exists" key.
         Writes (to stdout) a JSON array with one dict for each entry.
         This JSON can be used as the source for a Github Action strategy:matrix:include to run a parallel job for each entry.
 
-        Example:
-          $ ./bin/add-repo-exists.py ./docs/temp.json 5 100 | jq .
+        Example: Process 1st 5 entries in temp.json and select with 50% chance
+          
+          $ ./bin/select_repos.py ./docs/temp.json 100 3 | jq .
+
           [
             {
-              "repo_name": "golden-ledger",
-              "repo_exists": true
+              "repo_name": "golden-ledger"
             },
             {
-              "repo_name": "atomic-ledger",
-              "repo_exists": true
-            },
-            {
-              "repo_name": "distributed-ledger",
-              "repo_exists": false
-            },
-            {
-              "repo_name": "immutable-ledger",
-              "repo_exists": false
-            },
-            {
-              "repo_name": "canonical-ledger",
-              "repo_exists": false
+              "repo_name": "distributed-ledger"
             }
         ]          
     """)
@@ -70,9 +57,7 @@ def add_repo_exists():
     for i in range(0, n):
         r = random.randint(1, 100)
         if r < chance:
-            repo = json_data[i]
-            repo["repo_exists"] = repo_exists(repo["repo_name"])
-            repos.append(repo)
+            repos.append(json_data[i])
     return repos
 
 
