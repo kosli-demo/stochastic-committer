dbce92e16c20dd2a0a40d5755a89aaaae8ad04a0
JonJagger <jon@kosli.com>
2026-04-11 16:14:13 +0100

    Ensure unselected repos remain unchanged in the kosli snapshot

--- bin/select_repos.py
+++ bin/select_repos.py
@@ -8,24 +8,30 @@ import sys
 def print_help():
     print("""
         argv[1] == JSON filename containing list of repo names
-        argv[2] == number of entries in JSON filename to process
+        argv[2] == number of entries in JSON filename to select
         argv[3] == %chance (1-100) that each entry will be selected
           
         Writes (to stdout) a JSON array with one dict for each entry.
         This JSON can be used as the source for a Github Action strategy:matrix:include to run a parallel job for each entry.
 
-        Example: Process 1st 5 entries in temp.json and select with 50% chance
+        Example: Process 1st 3 entries in temp.json and select with 50% chance
           
-          $ ./bin/select_repos.py ./docs/temp.json 5 50 | jq .
+          $ ./bin/select_repos.py ./docs/temp.json 3 50 | jq .
 
           [
             {
-              "repo_name": "golden-ledger"
+              "repo_name": "data-adapter",
+              "selected": false
             },
             {
-              "repo_name": "distributed-ledger"
+              "repo_name": "golden-ledger",
+              "selected": true
+            },
+            {
+              "repo_name": "commercial-loan",
+              "selected": true
             }
-        ]          
+        ]      
     """)
 
 
@@ -39,9 +45,9 @@ def select_repos():
 
     repos = []
     for i in range(0, n):
-        r = random.randint(1, 100)
-        if r <= chance:
-            repos.append(json_data[i])
+        selected = random.randint(1, 100) <= chance
+        json_data[i]["selected"] = selected
+        repos.append(json_data[i])
     return repos
 
 
